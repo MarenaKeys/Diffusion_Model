47bdb85a4098fc7f0edc842c6c9c1710b18c328c
MarenaKeys <mskeys@andrew.cmu.edu>
2024-06-08 19:58:54 -0400

    Create Index.py

---
+++ Tensor_Indexing/Index.py
@@ -0,0 +1,10 @@
+#Indexing 
+import torch
+
+batch_size= 10
+features = 25
+x = torch.rand((batch_size, features))
+g = open("Indexing.log", 'a')
+print(x[0].shape, file = g) #gives all features # x[0,:]
+print(x[:,0].shape, file =g )#1 exa
+print(x[2,0:10], file =g ) #0:10 makes list from 0 to 9 from 3rd row
